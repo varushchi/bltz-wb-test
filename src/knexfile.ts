689e9a59f170de494f04d3ae97444cc34cd871d3
varushchi <vadim.shchigolev@mail.ru>
2025-08-18 21:48:41 +0300

    minor changes

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -5,10 +5,10 @@ const knexConfig: { [key: string]: Knex.Config } = {
   development: {
     client: "postgresql",
     connection: {
-      host: process.env.POSTGRES_HOST || "db",
-      database: process.env.POSTGRES_DB,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST || "localhost",
+      database: process.env.POSTGRES_DB || "postgres",
+      user: process.env.POSTGRES_USER || "postgres",
+      password: process.env.POSTGRES_PASSWORD || "postgres",
       port: Number(process.env.POSTGRES_PORT) || 5432,
     },
     pool: { min: 2, max: 10 },
